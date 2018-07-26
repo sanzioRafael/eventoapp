be7a279b6245b0a05d3ab5c10156b1e0b4454f57
Rafael Sanzio <rafael.sanzio18@gmail.com>
2018-07-26 15:09:54 -0400

    Configurando postgresql heroku parte 3

--- src/main/java/com/eventoapp/eventoapp/PostgreSqlConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/PostgreSqlConfiguration.java
@@ -4,9 +4,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.dialect.MySQLDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -14,40 +14,13 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import ch.qos.logback.core.db.dialect.PostgreSQLDialect;
 
 @Configuration
-public class DataConfiguration {
-
-	/*@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-		dataSource.setUsername("root");
-		dataSource.setPassword("Vostro*1");
-
-		return dataSource;
-	}*/
-
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-
-		/*adapter.setDatabase(Database.MYSQL);*/
-		adapter.setDatabase(Database.POSTGRESQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		System.out.println(MySQLDialect.class.getName());
-		System.out.println(PostgreSQLDialect.class.getName());
-		adapter.setDatabasePlatform(PostgreSQLDialect.class.getName());
-		adapter.setPrepareConnection(true);
-
-		return adapter;
-	}
+@Profile(value = "prod")
+public class PostgreSqlConfiguration {
 
 	@Bean
 	public BasicDataSource dataSource() throws URISyntaxException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-		
+
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
@@ -59,6 +32,19 @@ public class DataConfiguration {
 		basicDataSource.setPassword(password);
 
 		return basicDataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+		adapter.setDatabase(Database.POSTGRESQL); 
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform(PostgreSQLDialect.class.getName());
+		adapter.setPrepareConnection(true);
+
+		return adapter;
 	}
 
 }
