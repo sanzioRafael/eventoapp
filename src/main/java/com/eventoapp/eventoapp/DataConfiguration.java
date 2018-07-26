054986b25c937ee074be258e1202f92c360664d4
Rafael Sanzio <rafael.sanzio18@gmail.com>
2018-07-26 14:55:35 -0400

    Configurando postgresql heroku parte 2

--- src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -4,8 +4,14 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.dialect.MySQLDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import ch.qos.logback.core.db.dialect.PostgreSQLDialect;
 
 @Configuration
 public class DataConfiguration {
@@ -20,25 +26,28 @@ public class DataConfiguration {
 		dataSource.setPassword("Vostro*1");
 
 		return dataSource;
-	}
+	}*/
 
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter() {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 
-		adapter.setDatabase(Database.MYSQL);
+		/*adapter.setDatabase(Database.MYSQL);*/
+		adapter.setDatabase(Database.POSTGRESQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		System.out.println(MySQLDialect.class.getName());
+		System.out.println(PostgreSQLDialect.class.getName());
+		adapter.setDatabasePlatform(PostgreSQLDialect.class.getName());
 		adapter.setPrepareConnection(true);
 
 		return adapter;
-	}*/
+	}
 
 	@Bean
 	public BasicDataSource dataSource() throws URISyntaxException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
+		
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
